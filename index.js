a274efa7906e97db701862b1887554fa1c963e73
Arooj Chaudhry <s204759@student.dtu.dk>
2023-03-22 21:18:03 +0100

    couple of changes for testing purposes

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const redis = require("redis");
 const cors = require("cors");
+const sql = require("mssql");
 let RedisStore = require("connect-redis").default;
+
 
 
 mongoose.set('strictQuery', false);
@@ -74,11 +76,40 @@ app.use(
     },
   })
 );
+/*const config = {
+    user: 'sa',
+    password: 'miepStrong(!)Password',
+    server: 'localhost, 57000',
+    database: 'dev_testdb1',
+    options: {
+        enableAirthAbort: true
+    } 
+}
+console.log("it comes here");
+const run = async() => {
+    let pool;
+    try {
+        console.log('Connection Opening...');
+        pool = await new sql.ConnectionPool(config);
+        const {recordset} = await sql.query `select * from users;`;
+
+        console.log(recordset);
+    
+    } catch (err) {
+    console.log(err)
+    } finally{
+    await pool.close();
+    console.log('Connection closed');
+    }
+}
+
+run();
+console.log("sql runs here");*/
 
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
-  res.send("Hi  There!");
+  res.send("Hi  There!!");
   console.log("yeah it ran");
 });
 
