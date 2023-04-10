3b980a5d3454d30764f430cb011d9dafd3c17b0b
Arooj Chaudhry <s204759@student.dtu.dk>
2023-04-10 16:15:22 +0200

    updated getNeed api

--- controllers/needController.js
+++ controllers/needController.js
@@ -15,11 +15,12 @@ async function createNeed(req, res) {
   const id = await model.insertNeed({ NeedIs, Title, ContactPerson, FileData, originalname, extension, createdAt });
   return res.json({ id });
 }
-
+/*
 async function getNeed(req, res) {
+  const { NeedIs, Title, ContactPerson } = req.body;
   const { id } = req.params;
   const need = await model.getNeedById(id);
-  console.log('need:', need);
+
   if (!need) {
     return res.status(404).json({ message: 'Need not found' });
   }
@@ -28,9 +29,43 @@ async function getNeed(req, res) {
     const contentType = mime.getType('.' + need.extension);
     res.setHeader('Content-Type', contentType);
     res.setHeader('Content-Disposition', `attachment; filename=${need.originalname}`);
-    return res.send(fileData);
+    return res.json({fileData});
   }
-  return res.json(need);
+  else {
+    return res.json(need);
+  }
+}
+*/
+async function getNeed(req, res) {
+  const { id } = req.params;
+  const need = await model.getNeedById(id);
+
+  if (!need) {
+    return res.status(404).json({ message: 'Need not found' });
+  }
+
+  const responseBody = {
+    ID: need.ID,
+    NeedIs: need.NeedIs,
+    Title: need.Title,
+    ContactPerson: need.ContactPerson,
+    CreatedAt: need.CreatedAt,
+    OriginalName: need.originalname,
+    Extension: need.extension,
+    FileData: null
+  };
+
+  if (need.FileData) {
+    responseBody.FileData = Buffer.from(need.FileData, 'base64');
+    const contentType = mime.getType('.' + need.extension);
+    res.status(200).set({
+      'Content-Type': contentType,
+      'Content-Disposition': `inline; filename=${need.originalname}`
+    }).send(responseBody);
+    console.log(responseBody);
+  } else {
+    res.status(200).json(responseBody);
+  }    
 }
 
 module.exports = {
