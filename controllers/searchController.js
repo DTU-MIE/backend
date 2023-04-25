f76d9932bde58df2f9e53dd6457dd52885b433fd
Arooj Chaudhry <s204759@student.dtu.dk>
2023-04-25 15:56:31 +0200

    added some comments for search api

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -19,17 +19,17 @@ const dbConfig = {
 
 };  
 const search = async (req, res) => {
-    // Get the search keyword from the query string
+    //Getting the search keyword from the query 
     const keyword = req.query.keyword;
   
-    // Get the page number and results per page from the query string
+    //Getting the page number and results per page from the query
     const pageNumber = req.query.pageNumber || 1;
     const resultsPerPage = req.query.resultsPerPage || 10;
   
     try {
       const pool = await sql.connect(dbConfig);
   
-      // Get the names of all tables in the database
+      // Getting the names of all base tables in the database
       const tablesResult = await pool
         .request()
         .query(`
@@ -38,12 +38,12 @@ const search = async (req, res) => {
           WHERE TABLE_TYPE = 'BASE TABLE'
         `);
   
-      // Loop through each table and generate a search query
+      //promise to use it later and Loop through each table and generate a search query
       const promises = tablesResult.recordset.map(async (tableRow) => {
         const tableName = tableRow.TABLE_NAME;
   
-        // Get the names of all columns in the current table
-        const columnsR = await pool
+        //Getting the names of all columns in the current table
+        const columnsNames = await pool
           .request()
           .input('tableName', sql.NVarChar, tableName)
           .query(`
@@ -52,23 +52,28 @@ const search = async (req, res) => {
             WHERE TABLE_NAME = @tableName
           `);
   
-        // Build a dynamic SQL query that searches all columns in the current table
-        const columnsString = columnsR.recordset
+        //dynamic SQL query that searches all columns in the current table
+
+    //table name passed as paramter to prevent from sql injection
+        const columnsString = columnsNames.recordset
           .map((columnRow) => {
             const columnName = columnRow.COLUMN_NAME;
             return `${tableName}.${columnName} LIKE @keyword`;
           })
           .join(' OR ');
+        // we only need these from the table so it is hardcoded
+        //order by latest date first
         const searchQuery = `
-          SELECT ${tableName}.ContactPerson, ${tableName}.Title, ${tableName}.NeedIs, ${tableName}.id, CONCAT('${req.protocol}://${req.headers.host}/api/v1/download/', ${tableName}.id) AS fileURL
+          SELECT ${tableName}.ContactPerson, ${tableName}.Title, ${tableName}.NeedIs, ${tableName}.id, ${tableName}.CreatedAt, CONCAT('${req.protocol}://${req.headers.host}/api/v1/download/', ${tableName}.id) AS fileURL
           FROM ${tableName}
           WHERE ${columnsString}
-          ORDER BY CreatedAt
+          ORDER BY CreatedAt DESC 
           OFFSET ${(pageNumber - 1) * resultsPerPage} ROWS
           FETCH NEXT ${resultsPerPage} ROWS ONLY
         `;
   
-        // Execute the search query with the search keyword as a parameter
+        //Executing the search query with the search keyword as a parameter from the client 
+        //keyword passed as parameter to save it from sql injections
         const searchResult = await pool
           .request()
           .input('keyword', sql.NVarChar, `%${keyword}%`)
@@ -82,10 +87,9 @@ const search = async (req, res) => {
       const searchResults = await Promise.all(promises);
       const flattenedResults = searchResults.flat();
   
-      // Send the combined search results back to the client
+      // Send the combined search results back to the client 
       res.send(flattenedResults);
     } catch (err) {
-      // Handle any errors that occur during the search process
       console.error(err);
       res.status(500).send({ error: 'Internal Server Error' });
     }
