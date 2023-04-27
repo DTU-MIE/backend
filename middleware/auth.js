a6258733fbeb20eb3ae98181bb1944dacdd12218
Arooj Chaudhry <s204759@student.dtu.dk>
2023-04-27 08:07:28 +0200

    fixed type error

--- middleware/auth.js
+++ middleware/auth.js
@@ -31,7 +31,7 @@ async function authenticate(req, res, next) {
     }
 
    
-    let role = user.profession === 'Health Care Professional' ? 'Admin' : 'User';
+    const role = user.profession === 'Health Care Professional' ? 'Admin' : 'User';
     if (user.profession === 'student') {
       role = 'User';
     }
