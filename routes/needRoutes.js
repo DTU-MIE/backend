15d7dd11133d53b1b37c25b8994280cf16675a7e
Arooj Chaudhry <s204759@student.dtu.dk>
2023-05-01 10:42:29 +0200

    authentication added again

--- routes/needRoutes.js
+++ routes/needRoutes.js
@@ -7,8 +7,8 @@ const { authenticateToken} = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/needs', authenticateToken, upload.single('FileData'), needController.createNeed);
-router.get('/needs/:id', authenticateToken,  needController.getNeed);
-router.get('/download/:id',authenticateToken, needController.downloadFile);
+router.get('/needs/:id', authenticateToken, needController.getNeed);
+router.get('/download/:id', authenticateToken, needController.downloadFile);
 router.get('/allneeds', authenticateToken, needController.allNeeds);
 
 module.exports = router;
