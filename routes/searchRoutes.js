066686cccd6d2f27f4641ff200b8770b62064ae4
Arooj Chaudhry <s204759@student.dtu.dk>
2023-05-01 09:24:48 +0200

    authentication added

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,9 +2,9 @@ const sql = require("mssql");
 const express = require('express');
 const searchController = require('../controllers/searchController');
 const router = express.Router();
+const { authenticateToken} = require('../middleware/auth');
 
-
-router.get('/search', searchController.search);
+router.get('/search', authenticateToken, searchController.search);
 
 
 module.exports = router;
