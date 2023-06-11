3b5772f316a66c75b792c7991ff4275b7c9903cc
Arooj Chaudhry <s204759@student.dtu.dk>
2023-06-11 20:27:05 +0200

    added some things for production

--- routes/needRoutes.js
+++ routes/needRoutes.js
@@ -5,12 +5,12 @@ const upload = require('../middleware/fileUpload');
 const router = express.Router();
 
 
-router.post('/needs',  upload.single('FileData'), needController.createNeed);
-router.get('/allneeds',  needController.allNeeds);
-router.get('/needs/:id',  needController.getNeed);
-router.get('/download/:id', needController.downloadFile);
-router.put('/update/needs/:id', upload.single('FileData'), needController.updated);
-router.delete('/delete/:id', needController.deleted);
+router.post('/needs', authenticateToken, upload.single('FileData'), needController.createNeed);
+router.get('/allneeds', needController.allNeeds);
+router.get('/needs/:id', needController.getNeed);
+router.get('/download/:id',  needController.downloadFile);
+router.put('/update/needs/:id', authenticateToken, upload.single('FileData'), needController.updated);
+router.delete('/delete/:id', authenticateToken, needController.deleted);
 
 
 
